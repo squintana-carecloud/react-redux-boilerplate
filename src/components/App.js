a46db433291ca5b4e87136eb23a672c6115b7945
Santiago Quintana <squintana@carecloud.com>
2017-03-27 12:41:25 -0400

    Add router

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+const Home = () => (
+  <div className="App-header">
+    <h2>CareCloud React-Redux Boilerplate</h2>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>CareCloud React-Redux Boilerplate</h2>
-        </div>
+        <Router>
+          <Route exact path="/" component={Home}/>
+        </Router>
       </div>
-    );
+    )
   }
 }
 
--- src/components/Root.js
+++ src/components/Root.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
+
 import configureStore from '../configureStore'
 import App from './App'
 
 const store = configureStore()
 
-export default class Root extends Component {
+class Root extends Component {
   render() {
     return (
       <Provider store={store}>
@@ -14,3 +15,5 @@ export default class Root extends Component {
     )
   }
 }
+
+export default Root
